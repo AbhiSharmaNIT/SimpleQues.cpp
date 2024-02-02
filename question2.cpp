df29b67b1efcc7274058aa399bf79befa59db19f
Abhishek Kumar <136017324+AbhiSharmaNIT@users.noreply.github.com>
2024-02-02 23:22:49 +0530

    Add files via upload

---
+++ question2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <cmath>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+    
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    
+    int num1 = arr[n-1];
+    int num2 = arr[n-2];
+
+    string str1 = to_string(num1);
+    string str2 = to_string(num2);
+
+    cout<<"k"+str2+str1;
+
+}
